5323a5a776210834a6309ac26af9d8c0eb967ec3
SoulPoet <704936510@qq.com>
2017-04-14 20:26:15 +0800

    update_20170414_2026

--- SourceCreate.cpp
+++ SourceCreate.cpp
@@ -13,3 +13,8 @@ void SourceCreate::CreateBitStream(int *bit_stream, int len) {
 	for(int i = 0; i < len; ++i)
 		bit_stream[i] = (Uniform() < 0.5 ? 0 : 1);
 }
+
+void SourceCreate::CreateIntStream(int *int_stream, int len, int max_int) {
+	for(int i = 0; i < len; ++i)
+		int_stream[i] = Uniform() * (max_int + 1);
+}
