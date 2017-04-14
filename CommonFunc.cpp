ace44b2bcf2d055a56766c66af29d36644d6bbc7
SoulPoet <ZLM@ZLMMAC.local>
2017-04-14 11:26:02 +0800

    update_20170414

--- CommonFunc.cpp
+++ CommonFunc.cpp
@@ -1,5 +1,12 @@
 #include "CommonFunc.h"
 
+const int A = 48271;
+const long M = 2147483647;
+const int Q = M / A;
+const int R = M % A;
+long int state;
+bool normal_flag;
+
 CodeWord::CodeWord(int codeword_len) {
     len = codeword_len;
     cw = new int[codeword_len];
@@ -16,12 +23,41 @@ CodeWord CodeWord::operator + (const CodeWord &t) const {
     return rst;
 }
 
+void SetSeed(int flag) {
+    if (flag < 0)
+        state = 17;
+    else if (flag == 0) {
+        state = 0;
+        while (state == 0) {
+            srand((unsigned)time(NULL));
+            state = rand();
+        }
+    } else {
+        fprintf(stdout, "\nEnter the initial state: ");
+        fscanf(stdin, "%ld", &state);
+    }
+    normal_flag = false;
+}
+
+void PrintState(FILE *fp) {
+    fprintf(fp, "\n***init_state = %ld***\n", state);
+}
+
 double Uniform(double a, double b, long int &seed) {
-	seed = 2045 * seed + 1;
-	seed %= 1048576;
-	double rst = seed / 1048576.0;
-	rst = a + (a - b) * rst;
+    seed = 2045 * seed + 1;
+    seed %= 1048576;
+    double rst = seed / 1048576.0;
+    rst = a + (a - b) * rst;
     return rst;
+}
+
+double Uniform() {
+    int tmpState = A * ( state % Q ) - R * ( state / Q );
+    if(tmpState >= 0) {
+        state = tmpState;
+    else
+        state = tmpState + M;
+    return state / (double)M;
 }
 
 double Gauss(double mean, double segma, long int &seed) {
@@ -30,6 +66,30 @@ double Gauss(double mean, double segma, long int &seed) {
 		rst += Uniform(0.0, 1.0, seed);
 	rst = mean + (rst - 6.0) * segma;
 	return rst;
+}
+
+double Normal(double mean, double segma) {
+    static double x1, x2, w;
+
+    if(normal_flag) {
+        normal_flag = false;
+        return mean + segma * x2 * w;
+    }
+
+    w = 2.0;
+    while (w > 1.0){
+        x1 = 2.0 * Uniform() - 1.0;
+        x2 = 2.0 * Uniform() - 1.0;
+
+        w = x1 * x1 + x2 * x2;
+    }
+
+    w = sqrt(-2.0 * log(w) / w);
+
+    normal_flag = true;
+
+    return mean + segma * x1 * w;
+
 }
 
 int HammingDistance(int *str1, int *str2, int len) {
@@ -53,14 +113,14 @@ double SNR(double sigal_power, double noise_power) {
     return 10.0 * log10(sigal_power / noise_power);
 }
 
-void Debug(int val) {
-    printf("\nDebug:%d\n", val);
+void Debug(char *var_name, int val) {
+    printf("\nDebug:%s=%d\n", var_name, val);
 }
 
-void Debug(double val) {
-    printf("\nDebug:%f\n", val);
+void Debug(char *var_name, double val) {
+    printf("\nDebug:%s=%f\n", var_name, val);
 }
 
-void Debug(char *str) {
-    printf("\nDebug:%s\n", str);
+void Debug(char *var_name, char *str) {
+    printf("\nDebug:%s=%s\n", var_name, str);
 }
--- LineEncoder.cpp
+++ LineEncoder.cpp
@@ -12,10 +12,23 @@ void Encoder::Init(const char *config_file_path) {
     char str[50] = "";
     FILE *in_file = OpenFile(config_file_path, "r");
     while(~fscanf(in_file, "%[a-z_]=", str)) {
-        if(!strcmp(str, "codeword_len"))
+        if(!strcmp(str, "codeword_len")) {
             fscanf(in_file, "%d", &codeword_len);
-        else if(!strcmp(str, "codeword_num"))
+        }
+        else if(!strcmp(str, "codeword_num")) {
             fscanf(in_file, "%d", &codeword_num);
+            int cw = codeword_num;
+            message_blk_len = 0;
+            cw >>= 1;
+            while(cw) {
+                ++message_blk_len;
+                cw >>= 1;
+            }
+        }
+        else if(!strcmp(str, "message_blk_len")) {
+            fscanf(in_file, "%d", &message_blk_len);
+            codeword_num = 1 << message_blk_len;
+        }
         else if(!strcmp(str, "codebook")) {
             codebook = new int*[codeword_num];
             for(int i = 0; i < codeword_num; ++i) {
@@ -31,15 +44,39 @@ void Encoder::Init(const char *config_file_path) {
 }
 
 void Encoder::Encode(int *in, int *out, int blk_num) {
-    int i, j, k, idx_out;
-    for(int i = 0; i < blk_num * codeword_len; ++i)
-        out[i] = 0;
+    int i, j, in_idx = 0, target_codeword_idx, out_idx = 0;
     for(i = 0; i < blk_num; ++i) {
-        for(j = 0; j < codeword_len; ++j) {
-            idx_out = i * codeword_len + j;
-            for(k = 0; k < codeword_num; ++k) {
-                out[idx_out] ^= in[i * codeword_num + k] * codebook[k][j];
+        target_codeword_idx = 0;
+        for(j = 0; j < message_blk_len; ++j, ++in_idx)
+            target_codeword_idx = (target_codeword_idx << 1) | in[in_idx];
+        for(j = 0; j < codeword_len; ++j, ++out_idx)
+            out[out_idx] = codebook[target_codeword_idx][j];
+    }
+}
+
+void Encoder::Decode4BPSK_AWGN(double *in, int *imd, int *out, int blk_num) {
+    int i, j, imd_idx = 0, m, dis, target_codeword_idx, out_idx = 0;
+    for(i = 0; i < blk_num * codeword_len; ++i) {
+        imd[i] = (in[i] > 0 ? 0 : 1);
+    }
+
+    for(i = 0; i < blk_num; ++i) {
+        m = HammingDistance(imd + imd_idx, codebook[0], codeword_len);
+        target_codeword_idx = 0;
+        for(j = 1; j < codeword_num; ++j) {
+            dis = HammingDistance(imd + imd_idx, codebook[j], codeword_len);
+            if(dis < m) {
+                target_codeword_idx = j;
+                m = dis;
             }
+        }
+
+        for(j = 0; j < codeword_len; ++j, ++imd_idx)
+            imd[imd_idx] = codebook[target_codeword_idx][j];
+
+        for(j = 0; j < message_blk_len; ++j, ++out_idx) {
+            out[out_idx] = target_codeword_idx & 1;
+            target_codeword_idx >>= 1;
         }
     }
 }
